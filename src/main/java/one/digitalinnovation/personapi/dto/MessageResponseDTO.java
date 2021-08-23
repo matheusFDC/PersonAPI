5e396c00ef7efc3552bc5ca0cfc825ef01b60f6b
Matheus <feitosa.matheus3@gmail.com>
2021-08-23 14:13:08 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapi/dto/MessageResponseDTO.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageResponseDTO {
+
+    private String message;
+
+
+}
